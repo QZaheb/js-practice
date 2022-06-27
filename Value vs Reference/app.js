dd7743d3ff853565474c2a58ba78f18d08105df6
Qais Zaheb <100469755+QZaheb@users.noreply.github.com>
2022-06-27 23:26:31 +0430

    Add files via upload

---
+++ Value vs Reference/app.js
@@ -0,0 +1,13 @@
+let number = 1;
+let number2 = number;
+number2 = 7;
+console.log(`the number is ${number}`);
+console.log(`the number2 is ${number2}`);
+
+let person = {
+name: "Qais"
+}
+let person2 = person.name;
+ person2 = "Zaheb"
+console.log(`pernson name is ${person.name}`);
+console.log(`pernson2 name is ${person2}`);
